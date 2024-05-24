1a3f83555ce146e55c299107ac5f4c8f02aead68
Lucas Santana Lemos <lslemos59@gmail.com>
2024-05-24 13:22:53 -0300

    #

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
+app_name = 'app'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls'))
+    path('', views.home, name='home')
 ]
+        
--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'title': 'Home'
+    }
+    return render(request, 'global/base.html', context)
+    
